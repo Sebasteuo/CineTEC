4726091cb451d2a2f5abd4679dea0794ff1dac9b
Bojorge <38023924+Bojorge@users.noreply.github.com>
2021-09-27 23:13:42 -0600

    Conexión a una base de datos de prueba

---
+++ Server/CineTEC-API/Controllers/EmpleadoController.cs
@@ -0,0 +1,155 @@
+using CineTEC_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+using System.Threading.Tasks;
+
+
+namespace CineTEC_API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EmpleadoController : ControllerBase
+  {
+
+    private readonly IConfiguration _configuration;
+
+    public EmpleadoController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    // GET: api/<EmpleadoController>
+    [HttpGet]
+    public JsonResult GetAll()
+    {
+      string query = @"
+          select eid, nombre
+          from empleado
+          ";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("PostgreSQLConnection");
+      NpgsqlDataReader myReader;
+      using (NpgsqlConnection myCon=new NpgsqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using(NpgsqlCommand myComand=new NpgsqlCommand(query, myCon))
+        {
+          myReader = myComand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult(table);
+    }
+
+    // GET api/<EmpleadoController>/5
+    [HttpGet("{id}")]
+    public JsonResult GetOne(int id)
+    {
+      string query = @"
+          select eid, nombre
+          from empleado
+          where eid = @eid
+          ";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("PostgreSQLConnection");
+      NpgsqlDataReader myReader;
+      using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using (NpgsqlCommand myComand = new NpgsqlCommand(query, myCon))
+        {
+          myComand.Parameters.AddWithValue("@eid", id);
+          myReader = myComand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult(table);
+    }
+
+    // POST api/<EmpleadoController>
+    [HttpPost]
+    public JsonResult Create(Empleado empleado)
+    {
+      string query = @"
+          insert into empleado(nombre)
+          values (@nombre)
+          ";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("PostgreSQLConnection");
+      NpgsqlDataReader myReader;
+      using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using (NpgsqlCommand myComand = new NpgsqlCommand(query, myCon))
+        {
+          myComand.Parameters.AddWithValue("@nombre",empleado.nombre);
+          myReader = myComand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult("Added Successfully");
+    }
+
+    // PUT api/<EmpleadoController>/5
+    [HttpPut]
+    public JsonResult Update(Empleado empleado)
+    {
+      string query = @"
+          update empleado
+          set nombre = @nombre
+          where eid = @eid
+          ";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("PostgreSQLConnection");
+      NpgsqlDataReader myReader;
+      using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using (NpgsqlCommand myComand = new NpgsqlCommand(query, myCon))
+        {
+          myComand.Parameters.AddWithValue("@eid", empleado.eid);
+          myComand.Parameters.AddWithValue("@nombre", empleado.nombre);
+          myReader = myComand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult("Updated Successfully");
+    }
+
+    // DELETE api/<EmpleadoController>/5
+    [HttpDelete("{id}")]
+    public JsonResult Delete(int id)
+    {
+      string query = @"
+          delete from empleado
+          where eid = @eid
+          ";
+      DataTable table = new DataTable();
+      string sqlDataSource = _configuration.GetConnectionString("PostgreSQLConnection");
+      NpgsqlDataReader myReader;
+      using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+      {
+        myCon.Open();
+        using (NpgsqlCommand myComand = new NpgsqlCommand(query, myCon))
+        {
+          myComand.Parameters.AddWithValue("@eid", id);
+          myReader = myComand.ExecuteReader();
+          table.Load(myReader);
+          myReader.Close();
+          myCon.Close();
+        }
+      }
+      return new JsonResult("Deleted Successfully");
+    }
+  }
+}
--- Server/CineTEC-API/Startup.cs
+++ Server/CineTEC-API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace CineTEC_API
 {
@@ -26,17 +20,27 @@ namespace CineTEC_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+             c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+            .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CineTEC_API", Version = "v1" });
-            });
+            });                  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
