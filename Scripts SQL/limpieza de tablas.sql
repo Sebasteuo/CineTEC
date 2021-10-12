7939ca29c6b2e94b659c775dcb345e30d223d083
gabriel conejo <gacova95@gmail.com>
2021-10-12 09:54:43 -0600

    actualizacion de scripts
    destruccion: se agrega la limepiza sola de informacion de  las tablas(no correr todo el script posible error ) escoqger que se va a usar
    poblacion: se elimina loa comandos de insercion de la tabla butacaxfuncion debido a que esta solo se inserta en tiempo de ejecucion no antes

--- Scripts SQL/limpieza de tablas.sql
+++ Scripts SQL/limpieza de tablas.sql
@@ -1,3 +1,4 @@
+/*Destruir todo*/
 DROP TABLE public.rolxempleado;
 DROP TABLE public.rol;
 DROP TABLE public.protagonista;
@@ -14,3 +15,21 @@ DROP TABLE public.sala;
 DROP TABLE public.pelicula;
 DROP TABLE public.empleado;
 DROP TABLE public.sucursal;
+
+/*solo eleminar*/
+DELETE FROM public.rolxempleado;
+DELETE FROM public.rol;
+DELETE FROM public.protagonista;
+DELETE FROM public.director;
+DELETE FROM public.clasificacion;
+DELETE FROM public.facturaxempleado;
+DELETE FROM public.facturaxcliente;
+DELETE FROM public.cliente;
+DELETE FROM public.factura;
+DELETE FROM public.butacaxfuncion;
+DELETE FROM public.butaca;
+DELETE FROM public.funcion;
+DELETE FROM public.sala;
+DELETE FROM public.pelicula;
+DELETE FROM public.empleado;
+DELETE FROM public.sucursal;
